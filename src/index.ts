19c72142821c4514a431a23c54d83b92275c34c5
ImTemporaryHere <soapaka@live.com>
2024-08-13 18:03:27 +0300

    chore: mongoose added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
+import mongoose from 'mongoose'
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+
+// MongoDB Connection URI
+const mongoURI = process.env.MONGODB_URI as string;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoURI,);
+
+// Connection Events
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err:any) => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
